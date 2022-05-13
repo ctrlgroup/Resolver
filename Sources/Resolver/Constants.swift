c50f9e7ddac7d847aea0ed87db29e3487b5a19e2
andideheleanctrl <ad@ctrl-group.com>
2022-05-13 15:52:36 +0300

    Add constant for core data view context

---
+++ Sources/Resolver/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Resolver
+//
+//  Created by Andrei Dehelean on 13.05.2022.
+//
+
+import Foundation
+
+public extension Resolver.Name {
+  static let coreDataViewContext = Self("coreDataViewContext")
+}
